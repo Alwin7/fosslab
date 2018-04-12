aaf5802d6eb3a663b5a365d1b357054a131c0860
ALWIN SHAJI JOSEPH <36253945+Alwin7@users.noreply.github.com>
2018-04-12 08:01:46 +0530

    kernel compilation

--- kernel-compilation.sh
+++ kernel-compilation.sh
@@ -1,10 +1,6 @@
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.16.1.tar.xz
-mkdir linux
-cd linux
-mv linux-4.16.1.tar.xz ./linux
-tar xf linux-4.16.1.tar.xz
-cd linux-4.16.1
-cp /boot/config-$(uname -r) .config
+wget -q https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.16.1.tar.xz
+tar -xvJf linux-4.16.1.tar.xz
+cd linux-4.16.1.tar.xz
 sudo apt-get install fakeroot build-essential ncurses-dev xz-utils libssl-dev
-make menuconfig
+sudo make menuconfig
 sudo make -j 4 && sudo make modules_install -j 4 && sudo make install -j 4
